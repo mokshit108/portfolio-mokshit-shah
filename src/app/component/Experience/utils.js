63c11b13451dd22bc3ea694dd71ae6b9abe5b2d6
Mokshit <mokshit108@gmail.com>
2026-08-10 11:32:43 +0530

    feat:fixed console errors

--- src/app/component/Experience/utils.js
+++ src/app/component/Experience/utils.js
@@ -1,25 +1,25 @@
 import {
-    faReact, faVuejs, faJs, faCuttlefish, faHtml5,
-    faCss3Alt, faNode
-  } from '@fortawesome/free-brands-svg-icons';
-  import {
-    faCode, faFileCode, faDatabase, faServer
-  } from '@fortawesome/free-solid-svg-icons';
+  faReact, faVuejs, faJs, faCuttlefish, faHtml5,
+  faCss3Alt, faNode
+} from '@fortawesome/free-brands-svg-icons';
+import {
+  faCode, faFileCode, faDatabase, faServer
+} from '@fortawesome/free-solid-svg-icons';
 
-  export const iconMap = {
-    faReact, faVuejs, faJs, faCuttlefish, faHtml5,
-    faCss3Alt, faNode, faCode, faFileCode, faDatabase, faServer
+export const iconMap = {
+  faReact, faVuejs, faJs, faCuttlefish, faHtml5,
+  faCss3Alt, faNode, faCode, faFileCode, faDatabase, faServer
+};
+
+export const getTechIcon = (techName, techIcons, iconMap) => {
+  // Search for matching tech in our map (case-insensitive)
+  const tech = Object.keys(techIcons).find(
+    key => techName.toLowerCase().includes(key.toLowerCase())
+  );
+
+  const iconConfig = techIcons[tech] || techIcons.default;
+  return {
+    icon: iconMap[iconConfig.icon] || iconMap['faCode'],
+    color: iconConfig.color
   };
-
-  export const getTechIcon = (techName, techIcons, iconMap) => {
-    // Search for matching tech in our map (case-insensitive)
-    const tech = Object.keys(techIcons).find(
-      key => techName.toLowerCase().includes(key.toLowerCase())
-    );
-
-    const iconConfig = techIcons[tech] || techIcons.default;
-    return {
-      icon: iconMap[iconConfig.icon],
-      color: iconConfig.color
-    };
-  };
+};
